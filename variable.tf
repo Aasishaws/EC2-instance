066b0bdc045b0932521efaa5b6928ad27d403cfb
Aasishaws <115416682+Aasishaws@users.noreply.github.com>
2022-10-12 14:39:37 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,44 @@
+variable "region" {
+}
+variable "masterec2_ami_id" {
+}
+ variable "masterec2_instance_type" {
+}
+ variable "masterkey_name" {
+}
+ variable "master_ec2" {
+}
+ variable "slaveec2_ami_id" {
+}
+ variable "slaveec2_instance_type" {
+}
+ variable "slavekey_name" {
+}
+ variable "slave_ec2" {
+}
+variable "ingressdescription_a" {
+}
+variable "ingressfromport_a" {
+}
+variable "ingresstoport_a" {
+}
+variable "ingressprotocol_a" {
+}
+variable "ingressdescription_b" {
+}
+variable "ingressfromport_b" {
+}
+variable "ingresstoport_b" {
+}
+variable "ingressprotocol_b" {
+}
+variable "egressdescription" {
+}
+variable "egressfromport" {
+}
+variable "egresstoport" {
+}
+variable "egressprotocol" {
+}
+variable "sg" {
+}
